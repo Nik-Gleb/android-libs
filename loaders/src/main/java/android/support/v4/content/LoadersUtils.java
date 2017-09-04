0a25f4d828b784736c6ea30906dbaa8ebb39c46c
Nikitenko Gleb <nikitosgleb@gmail.com>
2017-09-04 08:11:00 +0300

    Release 0.0.1. Loaders small fix

---
+++ loaders/src/main/java/android/support/v4/content/LoadersUtils.java
@@ -0,0 +1,33 @@
+package android.support.v4.content;
+
+import android.support.annotation.NonNull;
+
+import proguard.annotation.Keep;
+import proguard.annotation.KeepPublicClassMembers;
+
+/**
+ * Common Loaders Utils.
+ *
+ * @author Nikitenko Gleb
+ * @since 1.0, 04/09/2017
+ */
+@Keep
+@KeepPublicClassMembers
+public final class LoadersUtils {
+
+  /**
+   * The caller should be prevented from constructing objects of this class.
+   * Also, this prevents even the native class from calling this constructor.
+   **/
+  private LoadersUtils() {
+    throw new AssertionError();
+  }
+
+  /**
+   * @param loader the loader instance
+   * @return true loader doing some bg
+   */
+  public static boolean isRunning(@NonNull AsyncTaskLoader loader)
+  {return loader.mTask != null;}
+
+}
