5bd72dc0058a761c748cce477ac5b1699f4fc4a7
Nikitenko Gleb <nikitosgleb@gmail.com>
2017-12-04 15:28:22 +0300

    Release: 1.2.3.4 Test tag2

--- mark.sh
+++ mark.sh
@@ -1,5 +1,6 @@
 git config --global user.email nikitosgleb@gmail.com && git config --global user.name "Gleb Nikitenko"
 git fetch && git checkout master && git merge dev && git push
 version=$(echo $GIT_COMMIT_DESC | grep -oP "[0-9].[0-9].[0-9].([0-9]|[0-9][0-9]|[0-9][0-9][0-9]|[0-9][0-9][0-9][0-9])")
+echo $GIT_COMMIT_DESC
 echo $version
 #
