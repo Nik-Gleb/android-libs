ffa98b375746fee903b21f8b2328325030c5524c
Nikitenko Gleb <nikitosgleb@gmail.com>
2017-12-05 14:45:22 +0300

    Release: 0.0.1.999 Tags added

--- mark.sh
+++
@@ -1,5 +0,0 @@
-#git config --global user.email nikitosgleb@gmail.com && git config --global user.name "Gleb Nikitenko"
-#git fetch && git checkout master && git merge dev && git push
-version=$(git log --format=oneline -n 1 $CIRCLE_SHA1 | grep -oP " [0-9].[0-9].[0-9].([0-9]|[0-9][0-9]|[0-9][0-9][0-9]|[0-9][0-9][0-9][0-9])")
-echo $version
-#
