4e521aa75f5dcafcecf6c1b6d5f7d4351bb4e693
Nikitenko Gleb <nikitosgleb@gmail.com>
2018-06-23 14:33:03 +0300

    Release: 0.0.1.0 Cleanup

---
+++ opengl/src/main/java/opengl/ColorFormat.java
@@ -0,0 +1,112 @@
+/*
+ * ColorFormat.java
+ * bundle-opengl
+ *
+ * Copyright (C) 2018, Gleb Nikitenko. All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package opengl;
+
+import android.graphics.Bitmap;
+import android.graphics.ImageFormat;
+import android.graphics.PixelFormat;
+import android.opengl.GLES20;
+import android.support.annotation.IntDef;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import static android.os.Build.VERSION.SDK_INT;
+import static android.os.Build.VERSION_CODES.M;
+
+/**
+ * Color Format of Graphic.
+ *
+ * @author Nikitenko Gleb
+ * @since 1.0, 17/12/2017
+ */
+@SuppressWarnings("unused")
+public enum ColorFormat {
+
+  /** RGBA-8888 Mode. */
+  TRANSPARENT(ColorMode.RGBA8888),
+  /** RGB-565 Mode. */
+  OPAQUE(ColorMode.RGB565);
+
+
+  /** {@link PixelFormat} */
+  final int pixelFormat;
+  /** {@link ImageFormat} */
+  final int imageFormat;
+  /** {@link Bitmap.Config} */
+  final Bitmap.Config bmpConfig;
+  /** GL Pixel Format */
+  final int glPixelFormat;
+
+  /** Color depths */
+  final int red, green, blue, alpha;
+  /** The native visual id */
+  final int nId;
+  /** Num of bytes per pixel. */
+  final int bytesPerPixel;
+
+  /** The gl-PixelType */
+  final int glPixelType;
+
+  /**
+   * Constructs a new {@link ColorFormat}
+   *
+   * @param mode the {@link ColorMode}
+   */
+  ColorFormat(@ColorMode int mode) {
+    switch (mode) {
+      case ColorMode.RGBA8888:
+        pixelFormat = PixelFormat.RGBA_8888;
+        imageFormat = SDK_INT >= M ? ImageFormat.FLEX_RGBA_8888 : 0x2A;
+        bmpConfig = Bitmap.Config.ARGB_8888;
+        glPixelType = GLES20.GL_UNSIGNED_BYTE;
+        glPixelFormat = GLES20.GL_RGBA; nId = 1;
+        red = 8; green = 8; blue = 8; alpha = 8;
+        break;
+      case ColorMode.RGB565:
+        pixelFormat = PixelFormat.RGB_565;
+        imageFormat = ImageFormat.RGB_565;
+        bmpConfig = Bitmap.Config.RGB_565;
+        glPixelType = GLES20.GL_UNSIGNED_SHORT_5_6_5;
+        glPixelFormat = GLES20.GL_RGB; nId = 4;
+        red = 5; green = 6; blue = 5; alpha = 0;
+        break;
+      default:
+        throw new IllegalArgumentException();
+    }
+    bytesPerPixel = ImageFormat.getBitsPerPixel(imageFormat) / 8;
+  }
+
+  /**
+   * Predefined color modes.
+   *
+   * @author Nikitenko Gleb
+   * @since 1.0, 06/10/2016
+   */
+  @IntDef({ColorMode.RGBA8888, ColorMode.RGB565})
+  @Retention(RetentionPolicy.SOURCE) private @interface ColorMode
+  {int RGBA8888 = 1, RGB565 = 4;}
+}
---
+++ opengl/src/main/java/opengl/EGLView.java
@@ -0,0 +1,282 @@
+/*
+ * EGLView.java
+ * bundle-opengl
+ *
+ * Copyright (C) 2018, Gleb Nikitenko. All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package opengl;
+
+import android.graphics.SurfaceTexture;
+import android.opengl.EGLSurface;
+import android.support.annotation.IntDef;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+
+import java.io.Closeable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.util.Objects;
+
+import static android.opengl.EGL14.EGL_HEIGHT;
+import static android.opengl.EGL14.EGL_WIDTH;
+import static opengl.EGLView.WindowType.HOLDER;
+import static opengl.EGLView.WindowType.OFFSCREEN;
+import static opengl.EGLView.WindowType.SURFACE;
+import static opengl.EGLView.WindowType.TEXTURE;
+
+/**
+ * Common base class for EGL surfaces.
+ * <p>
+ * There can be multiple surfaces associated with a single context.
+ *
+ * @author Nikitenko Gleb
+ * @since 1.0, 19/09/2017
+ */
+@SuppressWarnings({ "unused", "WeakerAccess", "SameParameterValue" })
+public final class EGLView implements Closeable {
+
+  /** The log-cat tag. */
+  private static final String TAG = "EGLView";
+
+  /** The EGL-Surface. */
+  @NonNull private final EGLSurface mEGLSurface;
+
+  /** Surface auto-release flag. */
+  private final boolean mRelease;
+  /** Platform surface */
+  private final Object mSurface;
+
+  /**
+   * EGLCore object we're associated with.
+   * It may be associated with multiple surfaces.
+   */
+  @NonNull public final EGLCore core;
+
+  /** The horizontal size. */
+  public final int width;
+  /** The vertical size. */
+  public final int height;
+
+  /** The type of surface. */
+  @SuppressWarnings("WeakerAccess")
+  @WindowType public final int type;
+
+  /**
+   * Constructs a new {@link EGLView}.
+   *
+   * @param core the egl-kernel
+   * @param surface the egl-surface
+   * @param type the type of window
+   * @param release window auto-release flag
+   */
+  private EGLView(@NonNull EGLCore core, @NonNull Object surface,
+      @WindowType int type, boolean release) {
+    mEGLSurface = (this.core = core).createSurface(surface);
+    width = this.core.querySurface(mEGLSurface, EGL_WIDTH);
+    height = this.core.querySurface(mEGLSurface, EGL_HEIGHT);
+    this.type = type; mSurface = surface; mRelease = release;
+  }
+
+  /**
+   * Constructs a new {@link EGLView}.
+   *
+   * @param core the egl-kernel
+   * @param width the horizontal size
+   * @param height the vertical size
+   * @param type the type of window
+   * @param release window auto-release flag
+   */
+  private EGLView(@NonNull EGLCore core, int width, int height,
+      @WindowType int type, boolean release) {
+    mEGLSurface = (this.core = core).createSurface
+        (this.width = width, this.height = height);
+    this.type = type; mSurface = null; mRelease = release;
+  }
+
+  /** {@inheritDoc} */
+  @Override public void close() {
+    core.releaseSurface(mEGLSurface);
+    if (mRelease) {
+      switch (type) {
+        case SURFACE:
+          ((Surface)mSurface).release();
+          break;
+        case TEXTURE:
+          ((SurfaceTexture)mSurface).release();
+          break;
+        case HOLDER:
+          (((SurfaceHolder)mSurface)).getSurface().release();
+        case OFFSCREEN:
+          // Nothing to do
+          break;
+      }
+    }
+  }
+
+  /** @return true if this view is current. */
+  @SuppressWarnings("BooleanMethodIsAlwaysInverted")
+  final boolean isCurrent() {return core.isCurrent(mEGLSurface);}
+
+  /** Makes our EGL context and surface current. */
+  public final void makeCurrent() {core.makeCurrent(mEGLSurface);}
+
+  /** Makes our EGL context and no-surface current. */
+  public final void makeNothing() {core.makeNothingCurrent();}
+
+  /**
+   * Makes our EGL context and surface current for drawing.
+   * Using the supplied surface for reading.
+   */
+  public final void makeCurrentReadFrom(@NonNull EGLView readSurface)
+  {core.makeCurrent(mEGLSurface, readSurface.mEGLSurface);}
+
+  /**
+   * Calls setPresentationTime and eglSwapBuffers.
+   * Use this to "publish" the current frame.
+   *
+   * @param nsecs presentation time stamp to EGL, in nanoseconds.
+   */
+  public final void swapBuffers(long nsecs) {
+    if (nsecs > 0) core.setPresentationTime(mEGLSurface, nsecs);
+    core.swapBuffers(mEGLSurface);
+  }
+
+  /**
+   * @param surface checkable surface
+   * @return true - if valid, otherwise - false
+   */
+  private static boolean isValid(@NonNull Surface surface)
+  {return surface.isValid();}
+
+  /**
+   * @param holder checkable surface holder
+   * @return true - if valid, otherwise - false
+   */
+  private static boolean isValid(@NonNull SurfaceHolder holder)
+  {return isValid(holder.getSurface());}
+
+  /**
+   * @param texture checkable surface texture
+   * @return true - if valid, otherwise - false
+   */
+  private static boolean isValid(@NonNull SurfaceTexture texture)
+  {return !OpenGL.isOreo() || !texture.isReleased();}
+
+  /** @return the type of hw-window. */
+  @WindowType
+  private static int getType(@Nullable Object obj) {
+    return (obj instanceof Surface && isValid((Surface) obj)) ? SURFACE :
+            (obj instanceof SurfaceHolder && isValid((SurfaceHolder) obj)) ? HOLDER :
+                (obj instanceof SurfaceTexture && isValid((SurfaceTexture) obj)) ? TEXTURE :
+                    EGLCore.error("Invalid surface object " + obj);
+  }
+
+  /**
+   * Constructs a new {@link EGLView}.
+   *
+   * @param core the core-instance
+   * @param width horizontal size of view
+   * @param height vertical size of view
+   * @param object the platform-window
+   * @param release window auto-release flag
+   *
+   * @return new created {@link EGLView}
+   */
+  @NonNull private static EGLView create(@NonNull EGLCore core,
+      int width, int height, @Nullable Object object, boolean release) {
+    return object == null ? new EGLView(core, width, height, OFFSCREEN, release) :
+        new EGLView(core, Objects.requireNonNull(object), getType(object), release);
+  }
+
+  /**
+   * Constructs a new off-screen egl-surface
+   *
+   * @param core the core-instance
+   * @param width horizontal size of view
+   * @param height vertical size of view
+   * @param release window auto-release flag
+   *
+   * @return new created {@link EGLView}
+   */
+  @NonNull public static EGLView offScreen
+      (@NonNull EGLCore core, int width, int height, boolean release)
+  {return create(core, width, height, null, release);}
+
+  /**
+   * Constructs a new hw-window egl-surface
+   *
+   * @param core the core-instance
+   * @param surface the platform-window
+   *
+   * @return new created {@link EGLView}
+   */
+  @NonNull public static EGLView surface
+  (@NonNull EGLCore core, @Nullable Surface surface, boolean release)
+  {return create(core, 0, 0, surface, release);}
+
+  /**
+   * Constructs a new hw-window egl-surface
+   *
+   * @param core the core-instance
+   * @param holder the platform-window
+   * @param release window auto-release flag
+   *
+   * @return new created {@link EGLView}
+   */
+  @NonNull public static EGLView holder
+  (@NonNull EGLCore core, @Nullable SurfaceHolder holder, boolean release)
+  {return create(core, 0, 0, holder, release);}
+
+  /**
+   * Constructs a new hw-window egl-surface
+   *
+   * @param core the core-instance
+   * @param texture the platform-window
+   * @param release window auto-release flag
+   *
+   * @return new created {@link EGLView}
+   */
+  @NonNull public static EGLView holder
+  (@NonNull EGLCore core, @Nullable SurfaceTexture texture, boolean release)
+  {return create(core, 0, 0, texture, release);}
+
+  /**
+   * Predefined Window-Types.
+   *
+   * @author Nikitenko Gleb
+   * @since 1.0, 06/10/2016
+   */
+  @IntDef({ OFFSCREEN, SURFACE, HOLDER, TEXTURE})
+  @Retention(RetentionPolicy.SOURCE)
+  @interface WindowType {
+    /** OFFSCREEN. */
+    int OFFSCREEN = -1;
+    /** {@link Surface}. */
+    int SURFACE = 0;
+    /** {@link SurfaceHolder}. */
+    int HOLDER  = 1;
+    /** {@link SurfaceTexture}. */
+    int TEXTURE = 2;
+  }
+}
